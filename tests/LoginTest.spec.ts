2b1e45137fb905396e597a34258178396355a63b
ramm-dev <ramm.get.dev@gmail.com>
2025-11-24 21:17:17 +0530

    pagechaining from loginpage to prodItem page

--- tests/LoginTest.spec.ts
+++ tests/LoginTest.spec.ts
@@ -1,5 +1,6 @@
 import {test,expect} from '@playwright/test';
 import { LoginPage } from '../pages/LoginPage';
+import { InventoryPage } from '../pages/InventoryPage';
 
 test.describe('Login Functionality Tests', ()=>{
 
@@ -18,4 +19,14 @@ test.describe('Login Functionality Tests', ()=>{
         await expect(errrMsg).toContain('Username and password do not match');
     });
 
+    test('should add to cart successfully', async ({page}) => {
+        let loginPage = new LoginPage(page);
+        await loginPage.goToLoginPage();
+        let inventoryPage = await loginPage.login('standard_user','secret_sauce');
+        await expect(page).toHaveTitle('Swag Labs');
+        // let inventoryPage = new InventoryPage(page);
+        let productItemPage = await inventoryPage.clickOnProduct();
+        await productItemPage.clickAddToCart();
+        await productItemPage.getItemPrice();
+    });
 });
